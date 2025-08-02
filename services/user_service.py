6950b02d2fccc870d35ce373ccc5cfc1ac4c399a
Robert Hodgkiss <188024351+wifiknight45@users.noreply.github.com>
2025-08-02 23:39:52 +0000

    Create user_service.py

---
+++ services/user_service.py
@@ -0,0 +1,27 @@
+class UserService:
+    """
+    Encapsulates all user-related operations:
+    - create_user
+    - get_user
+    - update_user
+    - delete_user
+    """
+
+    def __init__(self, repository):
+        self.repository = repository
+
+    def create_user(self, user_data):
+        # TODO: implement creation logic
+        pass
+
+    def get_user(self, user_id):
+        # TODO: implement retrieval logic
+        pass
+
+    def update_user(self, user_id, updates):
+        # TODO: implement update logic
+        pass
+
+    def delete_user(self, user_id):
+        # TODO: implement deletion logic
+        pass
